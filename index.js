d813b926772b3813ebe96e1c9ab5701c73a9cfbd
nanyr <nanyramirez0@gmail.com>
2017-03-20 17:21:09 -0400

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,33 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+const issuesWithUpdatedApiUrl= issues.map(function(issue){
+  return Object.assign({}, issue, {url: issue.url.replace('api.github.com', 'api-v2.github.com')});
+});
+
+const commentCountAcrossIssues= issues.map(function(issue){
+  return issue.comments_count;
+}).reduce(function(accumulator, currentVal){
+  return accumulator+currentVal;
+})
+
+const openIssues= issues.filter(function(issue){
+  return issue.state==="open";
+})
+
+const nonAutomaticIssues= issues.filter(function(issue){
+  return issue.body.includes('automatically created by learn.co')==false
+})
+
+function createTable(){
+  let tableB= document.getElementById('results');
+  nonAutomaticIssues.forEach(function(issue){
+    tableB.innerHTML+= `<tr>
+        <td> ${issue.body}</td>
+        <td>${issue.created_at}</td>
+        <td>${issue.state}</td>
+        </tr>`;
+  })
+}
+createTable();
